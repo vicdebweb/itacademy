5e49fb29919732f748135dbac8a5eba6f4577d0e
vialca3 <alcazar.caravaca.victor@gmail.com>
2025-11-11 10:30:15 +0100

    Ejercicio 2. Terminado.

--- sprint_1/tarea_2/nivel_1/index.php
+++ sprint_1/tarea_2/nivel_1/index.php
@@ -1,3 +1,7 @@
+// Victor Alcazar
+
+
+Ejercicio 1:
 <?php
 
 $integer = 10;
@@ -12,8 +16,27 @@ echo "Texto: $text, ";
 echo "Booleano: " . ($boolean ? "true" : "false") . "<br>";
 
 
-define("MI_NOMBRE", "VICTOR");
-
+define ("MI_NOMBRE", "VICTOR");
 echo "<h1>" . MI_NOMBRE . "</h1>";
+?>
+
+//Ejercicio 2:
+<?php
+define("Hi", " Hello World! ");
+echo Hi;
+
+echo strtoupper(Hi);
+echo strlen(Hi);
+echo strrev(Hi);
+
+$php = " Este es el curso de PHP";
+
+echo Hi, $php;
+?>
+
+//Ejercicio 3:
+<?php
+
+
 
 ?>
