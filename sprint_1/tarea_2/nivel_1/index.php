d5084543450d2bb175f7593cc3ef285e62bcfaa6
vialca3 <alcazar.caravaca.victor@gmail.com>
2025-11-11 17:43:17 +0100

    Ejercicio 4. Terminado.

--- sprint_1/tarea_2/nivel_1/index.php
+++ sprint_1/tarea_2/nivel_1/index.php
@@ -84,3 +84,13 @@ function Calcular ($x, $y, $operacion){
 Calcular (2, 8, "suma") . "\n";
 
 ?>
+
+/Ejercicio 4:
+<?php
+$n = 1; // El numero por el que empieza.
+
+while ($n <=30){ //Aquí cuenta hasta que la secuencia sea igual o menor que 30.
+	echo $n . "\n";
+	$n += 4; //Aquí cuenta de 4 en 4 números.
+}
+?>
