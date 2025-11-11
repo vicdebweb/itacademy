f010de5b72a07e83b161d743492bfacfef7f05e1
vialca3 <alcazar.caravaca.victor@gmail.com>
2025-11-11 11:16:01 +0100

    Ejercicio 3, parte a). Acabado.

--- sprint_1/tarea_2/nivel_1/index.php
+++ sprint_1/tarea_2/nivel_1/index.php
@@ -13,7 +13,7 @@ $boolean = true;
 echo "Entero: $integer, ";
 echo "Decimal: $double, ";
 echo "Texto: $text, ";
-echo "Booleano: " . ($boolean ? "true" : "false") . "<br>";
+echo "Booleano: " . ($boolean ? "true" : "false") . "\n";
 
 
 define ("MI_NOMBRE", "VICTOR");
@@ -37,6 +37,28 @@ echo Hi, $php;
 //Ejercicio 3:
 <?php
 
+$x = 2;
+$y = 4;
+$n = 2.22;
+$m = 4.44;
+
+echo "X = " . $x . ", Y = " . $y . "\n";
+echo 'Suma: ' . $x + $y . "\n";
+echo 'Resta: ' . $x - $y . "\n";
+echo 'Producto: ' . $x * $y . "\n";
+echo 'Módulo: ' . $x % $y . "\n";
+echo 'Doble de X: ' . $x * 2 . "\n";
+echo 'Doble de Y: ' . $y * 2 . "\n" . "\n";
 
 
+echo "N = " . $n . ", M = " . $m . "\n";
+echo 'Suma: ' . $n + $m . "\n";
+echo 'Resta: ' . $n - $m . "\n";
+echo 'Producto: ' . $n * $m . "\n";
+echo 'Módulo: ' . $n % $m . "\n";
+echo 'Doble de N: ' . $n * 2 . "\n";
+echo 'Doble de M: ' . $m * 2 . "\n" . "\n";
+
+echo 'Suma de todas las variables: ' . $x + $y + $n + $m . "\n";
+echo 'Producto de todas las variables: ' . $x * $y * $n * $m . "\n";
 ?>
