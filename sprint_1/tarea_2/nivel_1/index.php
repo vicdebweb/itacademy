45a4a56cec285f7c9fbde0470d49680abccb8c3d
vialca3 <alcazar.caravaca.victor@gmail.com>
2025-11-11 09:53:36 +0100

    Ejercicio 1. Terminado.

---
+++ sprint_1/tarea_2/nivel_1/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$integer = 10;
+$double = 3.14;
+$text = "Hola mundo";
+$boolean = true;
+
+
+echo "Entero: $integer, ";
+echo "Decimal: $double, ";
+echo "Texto: $text, ";
+echo "Booleano: " . ($boolean ? "true" : "false") . "<br>";
+
+
+define("MI_NOMBRE", "VICTOR");
+
+echo "<h1>" . MI_NOMBRE . "</h1>";
+
+?>
