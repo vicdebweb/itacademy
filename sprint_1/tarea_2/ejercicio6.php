c9d43ef0b54962c15749bb1a53d76e444aeecfef
vialca3 <alcazar.caravaca.victor@gmail.com>
2025-11-13 10:00:15 +0100

    Ejercicio 6 corregido a uso con funciones.

--- sprint_1/tarea_2/ejercicio6.php
+++ sprint_1/tarea_2/ejercicio6.php
@@ -1,9 +1,19 @@
 <?php
-$bite = rand(0, 1);
 
-if ($bite == 0) {
-    echo "TRUE";
-} else {
-    echo "FALSE";
+function isBitten() {
+    $bite = rand(0, 1);
+
+    if ($bite == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+if (isBitten()) {
+    echo "Charlie te ha mordido.";
+} else {
+    echo "Charlie ha fallado.";
+}
+
 ?>
